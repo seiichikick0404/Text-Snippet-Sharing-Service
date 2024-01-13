f995bb42d8a4ae4f026d48f5b7309d1d87d247b3
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-01-13 17:12:12 +0900

    ページへのルーティングを設定

--- src/Routing/routes.php
+++ src/Routing/routes.php
@@ -11,10 +11,14 @@ return [
         // データが必要な場合ここで取得
         return new HTMLRenderer('component/createSnippet');
     },
-    'api/random/part'=>function(): HTTPRenderer{
-        $part = DatabaseHelper::getRandomComputerPart();
-        return new JSONRenderer(['part'=>$part]);
+    'library'=>function(): HTTPRenderer{
+         // TODO: スニペット一覧をDBから取得する
+         return new HTMLRenderer('component/library');
     },
+    'show'=>function(): HTTPRenderer{
+        // TODO: 詳細情報をDBから取得する
+        return new HTMLRenderer('component/show');
+   },
     'api/parts'=>function(){
         $id = ValidationHelper::integer($_GET['id']??null);
         $part = DatabaseHelper::getComputerPartById($id);
